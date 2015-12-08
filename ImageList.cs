3fed9ac936ce5fd486d452dc5c4cb8558b231202
Redistributable <a32real@yahoo.co.jp>
2015-12-09 00:55:58 +0900

    コメント修正

--- ImageList.cs
+++ ImageList.cs
@@ -5,8 +5,11 @@ using System.Text;
 
 namespace a32system.CSProgram.AnimMaker
 {
+    /// <summary>
+    /// Image型のリスト
+    /// </summary>
     class ConvertImageList : List<Image>
     {
-        
+        // 追加実装なし
     }
 }
